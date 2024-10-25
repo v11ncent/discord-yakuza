30b54155c059c6c2dd211cff788b909773ee5b77
Vince <vince321c@gmail.com>
2024-10-24 22:25:12 -0400

    Added additional checks to make sure /interaction is ran from a server

--- src/commands/utility/initialize.ts
+++ src/commands/utility/initialize.ts
@@ -1,29 +1,48 @@
 // Fetches all messages & stores data in database
 import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 import { Admins } from "../../shared/enums/admins";
+import { Servers } from "../../shared/enums/servers";
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("initialize")
     .setDescription("Fetches & stores message data in the database."),
-  async execute(interaction: CommandInteraction) {
-    const userId = interaction.user.id;
 
-    if (!isAdmin(userId)) {
-      await interaction.reply(interaction.user.id);
+  async execute(interaction: CommandInteraction) {
+    if (isAllowed(interaction, true)) {
+      await interaction.reply(interaction.guildId as string);
     } else {
       await interaction.reply({
-        content: "You can't run command this unless you're an admin.",
+        content: "You can't run this command unless you're an admin 🤭",
         ephemeral: true,
       });
     }
   },
 };
 
-const isAdmin = (userId: string): boolean => {
-  if (Object.values(Admins).includes(userId)) {
-    return true;
+const isFromServer = (interaction: CommandInteraction): boolean => {
+  // https://discord.com/developers/docs/interactions
+  const GUILD_INTERACTION = 0;
+
+  return interaction.context === GUILD_INTERACTION;
+};
+
+const isAllowed = (
+  interaction: CommandInteraction,
+  onlyAdmins?: boolean,
+): boolean => {
+  if (!isFromServer(interaction)) {
+    return false;
   }
 
-  return false;
+  // Not sure why TS can't infer `guildId` type after the type guard
+  if (!Object.values(Servers).includes(interaction.guildId as string)) {
+    return false;
+  }
+
+  if (onlyAdmins) {
+    return Object.values(Admins).includes(interaction.user.id);
+  }
+
+  return true;
 };
