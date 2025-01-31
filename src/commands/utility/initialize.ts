cca98a9ac1ee2ed95b07f9e1cb4e396775c91156
Vince <vince321c@gmail.com>
2025-01-30 21:32:43 -0500

    Cleaned up message fetching

--- src/commands/utility/initialize.ts
+++ src/commands/utility/initialize.ts
@@ -10,11 +10,11 @@ import {
 } from "discord.js";
 import { isInteractionAllowed } from "../helpers/index";
 
-type Leaderboard = {
+type Ranking = {
   member: User;
   message: Message;
   count: number;
-}[];
+};
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -22,21 +22,11 @@ module.exports = {
     .setDescription("Fetches & stores message data in the database."),
 
   async execute(interaction: CommandInteraction) {
-    const guild = interaction.guild;
-
-    if (isInteractionAllowed(interaction) && guild) {
-      // Defer reply since getAllGuildMessages() takes longer than 3 seconds,
+    if (isInteractionAllowed(interaction) && interaction.guild) {
       // see: https://discordjs.guide/slash-commands/response-methods.html#editing-responses
-      await interaction.deferReply();
-
-      getAllGuildMessages(guild).then(async (leaderboard) => {
-        if (leaderboard) {
-          const first = leaderboard[0];
-          await interaction.editReply(
-            `Author: ${first?.member}\nMessage: ${first?.message}\nYakuza up count: ${first?.count}`,
-          );
-        }
-      });
+      await interaction.deferReply({ ephemeral: true });
+      const leaderboard = await initializeLeaderboard(interaction.guild);
+      await interaction.editReply(`Leaderboard initialized: ${leaderboard}`);
     } else {
       await interaction.reply({
         content: "You can't run this command unless you're an admin 🤭",
@@ -46,102 +36,137 @@ module.exports = {
   },
 };
 
-const getAllGuildMessages = async (guild: Guild) => {
-  const channels = await getAllGuildTextChannels(guild);
-  const channel = channels.values().next().value; // Get first for testing
+/**
+ * Create leaderboard with top rated messages
+ * @returns A leadboard of `Ranking`
+ */
+const initializeLeaderboard = async (guild: Guild) => {
+  const leaderboard: Ranking[] = [];
+  const messages = await getAllGuildMessages(guild);
 
-  if (channel) {
-    const leaderboard = await getAllMessagesFromChannel(channel);
+  console.log("Messages: " + messages.splice(0, 3));
+
+  if (messages) {
+    // messages.forEach((message) => {
+    //   leaderboard.push({
+    //     member: message.author,
+    //     message: message,
+    //     count: getMessageReactionCount(message),
+    //   });
+    // });
+
+    // leaderboard.sort((a, b) => b.count - a.count);
     return leaderboard;
   }
 
+  console.error("Error initializing leaderboard: `messages` is null.");
   return null;
 };
 
-// Get all messages in a channel
-// https://discordjs.guide/popular-topics/reactions.html#listening-for-reactions-on-old-messages
-const getAllMessagesFromChannel = async (channel: TextChannel) => {
-  let pack = await channel.messages.fetch({ limit: 100 });
-  let pivot = pack.last();
-  let messages = [...pack.values()].filter((message) => {
-    return message.author.bot === false;
-  });
-  let leaderboard: Leaderboard = [];
-  log(channel.name, messages.length);
+/**
+ * Get all messages from every `TextChannel` in a `Guild`
+ * @param guild A `Guild`
+ */
+const getAllGuildMessages = async (guild: Guild) => {
+  const channels = await getAllGuildTextChannels(guild);
 
-  while (pivot !== undefined && messages.length < 200) {
-    pack = await channel.messages.fetch({
-      limit: 100,
-      before: pivot.id,
-    });
-
-    const userMessages = [...pack.values()].filter((message) => {
-      return message.author.bot === false;
-    });
-
-    pivot = pack.last();
-    messages = [...messages, ...userMessages];
-    leaderboard = initializeLeaderboard(messages);
-
-    log(channel.name, messages.length);
+  if (channels) {
+    // Using `Promise.all()` here so that we get an array of data rather than promises
+    const messages = await Promise.all(channels.map(getAllChannelMessages));
+    return messages.flat();
   }
 
-  console.log(`${channel.name} channel message fetch operation finished.`);
-  // return messages;
-
-  return leaderboard;
-};
-
-// Update leaderboard -> Get all messages -> update every batch of 100
-
-// Update leaderboard on every iteration of getAllMessagesFromChannel()
-const initializeLeaderboard = (messages: Message[]): Leaderboard | [] => {
-  // Just grabbing the first for testing
-  const first = messages[0];
-
-  if (first) {
-    const count = getMessageReactionCount(first);
-
-    return [
-      {
-        member: first.author,
-        message: first,
-        count,
-      },
-    ];
-  }
-
+  console.error("Error fetching guild messages");
   return [];
 };
 
 /**
- * Get specific reaction counts on a message.
- * @param message A text message.
- * @param filter A list of reactions to filter for. Defaults to yakuza up.
- * @returns
+ * Gets all `TextChannel` in a `Guild`
+ * @param guild A `Guild`
+ * @returns A Collection of `TextChannel`
  */
-const getMessageReactionCount = (
-  message: Message,
-  filter: string[] = ["💹"],
-) => {
-  const [...all] = message.reactions.cache.values();
-
-  const find = all.filter((reaction) => {
-    const name = reaction.emoji.name ?? "";
-    return filter.includes(name);
-  });
-
-  return find[0]?.count ?? 0;
-};
-
 const getAllGuildTextChannels = async (guild: Guild) => {
   return guild.channels.cache.filter(
     (channel) => channel !== null && channel.type === ChannelType.GuildText,
   );
 };
 
-const log = (channelName: string, messagesLength: number) => {
-  console.log(
-    `Getting all messages from ${channelName}: ${messagesLength} / 1000...`,
-  );
+/**
+ * Get all messages from a `TextChannel`
+ * https://discordjs.guide/popular-topics/reactions.html#listening-for-reactions-on-old-messages
+ * @param channel A `TextChannel`
+ * @returns An array of `Message`
+ */
+const getAllChannelMessages = async (
+  channel: TextChannel,
+): Promise<Message[]> => {
+  let messages = await getQualifyingMessages(channel);
+  let pivot = messages.pop()?.id;
+
+  while (pivot !== undefined && messages.length < 100) {
+    let batch = await getQualifyingMessages(channel, pivot);
+    messages = [...messages, ...batch];
+    pivot = batch.pop()?.id;
+  }
+
+  return messages;
 };
+
+/**
+ * Filters an array of messages
+ * @param messages An array of `Message`
+ * @param pivot
+ * @returns An array of `Message` that are written by a user and contain text
+ */
+const getQualifyingMessages = async (
+  channel: TextChannel,
+  pivot?: string,
+): Promise<Message[]> => {
+  const batch = await channel.messages.fetch({
+    limit: 100,
+    ...(pivot && { before: pivot }), // Cool! Using spread to optionally include
+  });
+  let messages = Array.from(batch.values());
+
+  return messages.filter((message) => {
+    return isTextMessage(message) && !isBotMessage(message);
+  });
+};
+
+/**
+ * Determines if `Message` author was a bot
+ * @param message A `Message`
+ * @returns `boolean`
+ */
+const isBotMessage = (message: Message): boolean => {
+  return message.author.bot;
+};
+
+/**
+ * Determines if `Message` contains text.
+ * @param message
+ * @returns `boolean`
+ */
+const isTextMessage = (message: Message): boolean => {
+  return message.content !== "";
+};
+
+/**
+ * Get specific reaction counts on a message
+ * @param message A text message
+ * @param filter A list of reactions to filter
+ * @returns A `number` of filtered reactions
+ */
+// const getMessageReactionCount = (
+//   message: Message,
+//   filter: string[] = ["💹"],
+// ) => {
+//   const [...all] = message.reactions.cache.values();
+
+//   const find = all.filter((reaction) => {
+//     const name = reaction.emoji.name ?? "";
+//     return filter.includes(name);
+//   });
+
+//   return find[0]?.count ?? 0;
+// };
