9210077fa1a3ed722749b5405c88490f72e5e60f
Vince <vince321c@gmail.com>
2025-01-31 00:02:22 -0500

    Rewrote isInteractionAllowed helper

--- src/commands/helpers/index.ts
+++ src/commands/helpers/index.ts
@@ -1,25 +1,23 @@
-import { CommandInteraction, ChannelType, TextBasedChannel } from "discord.js";
+import { CommandInteraction } from "discord.js";
 import { Admins } from "../../shared/enums/admins";
 import { Servers } from "../../shared/enums/servers";
 
-// Checks if channel is of type `GuildText`
-export function isGuildTextChannel(channel: TextBasedChannel | null) {
-  return channel !== null && channel.type === ChannelType.GuildText;
-}
-
-// Checks if user is allowed to run interaction
+/**
+ * Checks if member is authorized to call interaction
+ * @param interaction The `CommandInteraction` to call
+ * @param onlyAdmins Authorize only admins to call the interaction
+ * @returns A `boolean`
+ */
 export const isInteractionAllowed = (
   interaction: CommandInteraction,
   onlyAdmins: boolean = true,
 ): boolean => {
-  // Not sure why TS can't infer `guildId` type after the type guard
-  if (!Object.values(Servers).includes(interaction.guildId as string)) {
-    return false;
-  }
+  const guild = interaction.guildId;
+  if (!guild) return false; // Return false if not in guild
 
   if (onlyAdmins) {
     return Object.values(Admins).includes(interaction.user.id);
   }
 
-  return true;
+  return !Object.values(Servers).includes(guild);
 };
--- src/commands/leaderboard/leaderboard.ts
+++ src/commands/leaderboard/leaderboard.ts
@@ -3,7 +3,13 @@ import { SlashCommandBuilder, CommandInteraction } from "discord.js";
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("leaderboard")
-    .setDescription("Displays the leaderboard."),
+    .setDescription("Displays the leaderboard.")
+    .addMentionableOption((option) =>
+      option
+        .setName("Member")
+        .setDescription("The member to lookup")
+        .setRequired(true),
+    ),
   async execute(interaction: CommandInteraction) {
     await interaction.reply({
       ephemeral: true,
