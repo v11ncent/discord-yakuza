173070090359ebf2b7ed1ec51fd26bd86d53ff7b
Vince <vince321c@gmail.com>
2025-01-30 19:49:02 -0500

    Very basic reaction counting

--- notes.md
+++ notes.md
@@ -3,10 +3,18 @@
 I want to fetch all messages from all channels in the guild that the `initialize` command was called from. We can't fetch messages from a specific user, so we need to sort through all the messages in a guild, and then filter by member.
 
 1. Grab all text channels from guild where command was called from,
-2. Grab all messages from each text channel,
+2. Grab all messages from each text channel, (done)
 3. Iterate through array of messages and filter by user,
 4. Save highest rated message for each user in database.
 
-# Misc
+## Update leaderboard
+
+To update the leaderboard, we need to grab the emoji count for each user:
+
+1. Get all messages, (done)
+2. Filter a single message for a list of emojis and get their respective count,
+3. Update leaderboard (?)
+
+## Misc
 
 Basically Guilds, Channels, Roles and PermissionOverwrites are all cached on ready with Guilds intent. The rest is cached when received from a gateway event
--- src/commands/utility/initialize.ts
+++ src/commands/utility/initialize.ts
@@ -5,8 +5,16 @@ import {
   Guild,
   ChannelType,
   TextChannel,
+  Message,
+  User,
 } from "discord.js";
 import { isInteractionAllowed } from "../helpers/index";
+
+type Leaderboard = {
+  member: User;
+  message: Message;
+  count: number;
+}[];
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -19,9 +27,15 @@ module.exports = {
     if (isInteractionAllowed(interaction) && guild) {
       // Defer reply since getAllGuildMessages() takes longer than 3 seconds,
       // see: https://discordjs.guide/slash-commands/response-methods.html#editing-responses
-      await interaction.deferReply({ ephemeral: true });
-      getAllGuildMessages(guild).then(async () => {
-        await interaction.editReply("Initialization completed !!");
+      await interaction.deferReply();
+
+      getAllGuildMessages(guild).then(async (leaderboard) => {
+        if (leaderboard) {
+          const first = leaderboard[0];
+          await interaction.editReply(
+            `Author: ${first?.member}\nMessage: ${first?.message}\nYakuza up count: ${first?.count}`,
+          );
+        }
       });
     } else {
       await interaction.reply({
@@ -34,9 +48,14 @@ module.exports = {
 
 const getAllGuildMessages = async (guild: Guild) => {
   const channels = await getAllGuildTextChannels(guild);
-  const channel = channels.values().next().value;
+  const channel = channels.values().next().value; // Get first for testing
 
-  if (channel) await getAllMessagesFromChannel(channel);
+  if (channel) {
+    const leaderboard = await getAllMessagesFromChannel(channel);
+    return leaderboard;
+  }
+
+  return null;
 };
 
 // Get all messages in a channel
@@ -44,31 +63,85 @@ const getAllGuildMessages = async (guild: Guild) => {
 const getAllMessagesFromChannel = async (channel: TextChannel) => {
   let pack = await channel.messages.fetch({ limit: 100 });
   let pivot = pack.last();
-  let messages = [...pack.values()];
+  let messages = [...pack.values()].filter((message) => {
+    return message.author.bot === false;
+  });
+  let leaderboard: Leaderboard = [];
+  log(channel.name, messages.length);
 
-  console.log(
-    `Getting all messages from ${channel.name}: ${messages.length} / 1000...`,
-  );
-
-  while (pivot !== undefined && messages.length < 10000) {
+  while (pivot !== undefined && messages.length < 200) {
     pack = await channel.messages.fetch({
       limit: 100,
       before: pivot.id,
     });
 
+    const userMessages = [...pack.values()].filter((message) => {
+      return message.author.bot === false;
+    });
+
     pivot = pack.last();
-    messages = [...messages, ...pack.values()];
-    console.log(
-      `Getting all messages from ${channel.name}: ${messages.length} / 1000...`,
-    );
+    messages = [...messages, ...userMessages];
+    leaderboard = initializeLeaderboard(messages);
+
+    log(channel.name, messages.length);
   }
 
   console.log(`${channel.name} channel message fetch operation finished.`);
-  return messages;
+  // return messages;
+
+  return leaderboard;
+};
+
+// Update leaderboard -> Get all messages -> update every batch of 100
+
+// Update leaderboard on every iteration of getAllMessagesFromChannel()
+const initializeLeaderboard = (messages: Message[]): Leaderboard | [] => {
+  // Just grabbing the first for testing
+  const first = messages[0];
+
+  if (first) {
+    const count = getMessageReactionCount(first);
+
+    return [
+      {
+        member: first.author,
+        message: first,
+        count,
+      },
+    ];
+  }
+
+  return [];
+};
+
+/**
+ * Get specific reaction counts on a message.
+ * @param message A text message.
+ * @param filter A list of reactions to filter for. Defaults to yakuza up.
+ * @returns
+ */
+const getMessageReactionCount = (
+  message: Message,
+  filter: string[] = ["💹"],
+) => {
+  const [...all] = message.reactions.cache.values();
+
+  const find = all.filter((reaction) => {
+    const name = reaction.emoji.name ?? "";
+    return filter.includes(name);
+  });
+
+  return find[0]?.count ?? 0;
 };
 
 const getAllGuildTextChannels = async (guild: Guild) => {
   return guild.channels.cache.filter(
     (channel) => channel !== null && channel.type === ChannelType.GuildText,
+  );
+};
+
+const log = (channelName: string, messagesLength: number) => {
+  console.log(
+    `Getting all messages from ${channelName}: ${messagesLength} / 1000...`,
   );
 };
